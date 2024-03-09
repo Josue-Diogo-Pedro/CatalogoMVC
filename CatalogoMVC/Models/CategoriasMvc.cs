bc7efdc234d516cd3c844d82e0dd06668ae0d59a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-09 13:15:13 -0800

    Creating View Model CategoriasMvc

---
+++ CatalogoMVC/Models/CategoriasMvc.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoMVC.Models;
+
+public class CategoriasMvc
+{
+    public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "O nome da categoria é obirgatório")]
+    public string Nome { get; set; }
+
+    [Required]
+    [Display(Name = "Imagem")]
+    public string ImagemUrl { get; set; }
+}
