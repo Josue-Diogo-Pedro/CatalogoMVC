0e61c08ad1659c7a342478552aeea7f2822e576a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-09 13:10:59 -0800

    Deleting Model Error and same texts inside on Error

--- CatalogoMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CatalogoMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
