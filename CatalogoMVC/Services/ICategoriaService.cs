7c1de10b78815d6288e394b3859a1294093a353c
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-09 23:42:05 -0800

    Finish to create interface for ICategoriaService

--- CatalogoMVC/Services/ICategoriaService.cs
+++ CatalogoMVC/Services/ICategoriaService.cs
@@ -4,5 +4,9 @@ namespace CatalogoMVC.Services;
 
 public interface ICategoriaService
 {
-    Task<IEnumerable<CategoriasMvc>> 
+    Task<IEnumerable<CategoriaViewModel>> GetCategorias();
+    Task<CategoriaViewModel> GetCategoriaById(int id);
+    Task<CategoriaViewModel> CreateCategoria(CategoriaViewModel categoriaVM);
+    Task<bool> UpdateCategoria(int id, CategoriaViewModel categoriaVM);
+    Task<bool> RemoveCategoria(int id);
 }
