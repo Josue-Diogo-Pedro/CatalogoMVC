3ac17c2d599a1433ecf79884f414427adfc2f241
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-10 14:57:01 -0700

    Implement action Index in CategoriasController

---
+++ CatalogoMVC/Controllers/CategoriasController.cs
@@ -0,0 +1,22 @@
+﻿using CatalogoMVC.Models;
+using CatalogoMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoMVC.Controllers;
+
+public class CategoriasController : Controller
+{
+    private readonly ICategoriaService _categoriaService;
+
+    public CategoriasController(ICategoriaService categoriaService) => _categoriaService = categoriaService;
+
+    public async Task<ActionResult<IEnumerable<CategoriaViewModel>>> Index()
+    {
+        var result = await _categoriaService.GetCategorias();
+
+        if (result is null) return View("Error");
+
+        return View(result);
+    }
+}
+
