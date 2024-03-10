2b3789400b0c3fe4b6514c2dc5443f524bc5cbd6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-09 23:48:11 -0800

    Create CategoriaService and the base of that implementation

---
+++ CatalogoMVC/Services/CategoriaService.cs
@@ -0,0 +1,47 @@
+﻿using CatalogoMVC.Models;
+using System.Text.Json;
+
+namespace CatalogoMVC.Services;
+
+public class CategoriaService : ICategoriaService
+{
+    private const string apiEndpoint = "/api/categorias";
+
+    private readonly JsonSerializerOptions _options;
+    private readonly IHttpClientFactory _clientFactory;
+
+    private CategoriaViewModel categoriaVM;
+    private IEnumerable<CategoriaViewModel> categoriasVM;
+
+    public CategoriaService(IHttpClientFactory clientFactory)
+    {
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<IEnumerable<CategoriaViewModel>> GetCategorias()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CategoriaViewModel> GetCategoriaById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CategoriaViewModel> CreateCategoria(CategoriaViewModel categoriaVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateCategoria(int id, CategoriaViewModel categoriaVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveCategoria(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
