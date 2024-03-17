61263377c29c37c13dbff8e4ce7d6b21eab42d91
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-17 03:35:43 -0700

    Creating AccountController to manage login and token of user in session

---
+++ CatalogoMVC/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using CatalogoMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoMVC.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IAutenticacao _autenticacaoService;
+
+	public AccountController(IAutenticacao autenticacaoService) => _autenticacaoService = autenticacaoService;
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+
+}
