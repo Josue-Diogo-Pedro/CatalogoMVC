86e31dae19e42759533b667bac10787da0d7d7f6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-17 02:23:34 -0700

    Create view model Usuario and Token

---
+++ CatalogoMVC/Models/TokenViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoMVC.Models;
+
+public class TokenViewModel
+{
+    public bool Authenticated { get; set; }
+    public DateTime Expiration { get; set; }
+    public string Token { get; set; }
+    public string Message { get; set; }
+}
---
+++ CatalogoMVC/Models/UsuarioViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CatalogoMVC.Models;
+
+public class UsuarioViewModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
