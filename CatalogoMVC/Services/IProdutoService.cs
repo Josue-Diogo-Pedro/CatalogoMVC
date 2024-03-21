6d8c3f961183fa8eb1648e36ae48a7aba7e6a1d9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-21 00:47:28 -0700

    Create contracts in IProdutoService

--- CatalogoMVC/Services/IProdutoService.cs
+++ CatalogoMVC/Services/IProdutoService.cs
@@ -1,6 +1,12 @@
-﻿namespace CatalogoMVC.Services
+﻿using CatalogoMVC.Models;
+
+namespace CatalogoMVC.Services;
+
+public interface IProdutoService
 {
-    public interface IProdutoService
-    {
-    }
+    Task<IEnumerable<ProdutoViewModel>> GetProdutos(string token);
+    Task<ProdutoViewModel> GetProdutoPorId(int id, string token);
+    Task<ProdutoViewModel> CreateProduto(ProdutoViewModel produtoVM, string token);
+    Task<bool> UpdateProduto(int id, ProdutoViewModel produtoVM, string token);
+    Task<bool> RemoveProduto(int id, string token);
 }
