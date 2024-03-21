8dd0e491fd474d96be95ebd87e1c74e3d9c9c1a4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-21 01:04:37 -0700

    Define properties and constructor

--- CatalogoMVC/Services/ProdutoService.cs
+++ CatalogoMVC/Services/ProdutoService.cs
@@ -1,6 +1,21 @@
-﻿namespace CatalogoMVC.Services
+﻿using CatalogoMVC.Models;
+using System.Text.Json;
+
+namespace CatalogoMVC.Services;
+
+public class ProdutoService
 {
-    public class ProdutoService
+    private readonly IHttpClientFactory _clientFactory;
+    private const string apiEndpoint = "api/produtos/";
+    private readonly JsonSerializerOptions _options;
+    private ProdutoViewModel _produtoVM;
+    private IEnumerable<ProdutoViewModel> _produtosVM;
+
+    public ProdutoService(IHttpClientFactory clientFactory)
     {
+        _clientFactory = clientFactory;
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
     }
+
+
 }
