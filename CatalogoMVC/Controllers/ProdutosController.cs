1da10b7c6c225f39b2b0f3bd4c5e009bf9a23e85
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-21 07:47:29 -0700

    Finish to implement action to open(GET) view of create product and action to save(POST) product

--- CatalogoMVC/Controllers/ProdutosController.cs
+++ CatalogoMVC/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogoMVC.Models;
 using CatalogoMVC.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CatalogoMVC.Controllers;
 
@@ -16,12 +17,37 @@ public class ProdutosController : Controller
         _produtoService = produtoService;
     }
 
+    [HttpGet]
     public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> Index()
     {
         var result = await _produtoService.GetProdutos(ObtemToken());
 
         if (result is null) return View("Error");
         else return View(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> CriarNovoProduto()
+    {
+        var categorias = await _categoriaService.GetCategorias();
+        ViewBag.CategoriaId = new SelectList(categorias, "CategoriaId", "Nome");
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProdutoViewModel>> CriarNovoProduto(ProdutoViewModel produtoVM)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _produtoService.CreateProduto(produtoVM, ObtemToken());
+
+            if(result  is not null) return RedirectToAction(nameof(Index));
+
+        }
+        else ViewBag.CategoriaId = new SelectList(await _categoriaService.GetCategorias(), "CategoriaId", "Nome");
+
+        return View(produtoVM);
     }
 
     private string ObtemToken()
