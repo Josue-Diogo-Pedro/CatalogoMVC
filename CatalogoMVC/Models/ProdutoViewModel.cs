7e18b17f35ae348b1d2dc28b19343784cf281a85
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-21 07:03:13 -0700

    Finish to create View Index and fix same errors

--- CatalogoMVC/Models/ProdutoViewModel.cs
+++ CatalogoMVC/Models/ProdutoViewModel.cs
@@ -10,7 +10,8 @@ public class ProdutoViewModel
     public string Nome { get; set; }
 
     [Required(ErrorMessage = "A descrição do Produto é obrigatória")]
-    public string Descricacao { get; set; }
+    [Display(Name = "Descrição")]
+    public string Descricao { get; set; }
 
     [Required(ErrorMessage = "Informe o preço do Produto")]
     public decimal Preco { get; set; }
