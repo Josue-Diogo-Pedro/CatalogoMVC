4a803448bcb4a47059f342a018b669a58a2b9e03
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-21 00:42:40 -0700

    Create ProdutoViewModel

---
+++ CatalogoMVC/Models/ProdutoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoMVC.Models;
+
+public class ProdutoViewModel
+{
+    public int ProdutoId { get; set; }
+
+    [Required(ErrorMessage = "O nome do Produto é obrigatório")]    
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição do Produto é obrigatória")]
+    public string Descricacao { get; set; }
+
+    [Required(ErrorMessage = "Informe o preço do Produto")]
+    public decimal Preco { get; set; }
+
+    [Required(ErrorMessage = "Informe o caminho da imagem do Produto")]
+    [Display(Name = "Caminho da Imagem")]
+    public string ImagemUrl { get; set; }
+
+    [Display(Name = "Categoria")]
+    public int CategoriaId { get; set; }
+}
